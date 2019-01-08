4a306045e2beb67ec163e02385acb5cf254bcd14
ruyonga <druyonga@gmail.com>
2019-01-08 09:54:03 +0300

    gen_statem states and callbacks

--- lib/game.ex
+++ lib/game.ex
@@ -1,7 +1,6 @@
 defmodule IslandsEngine.Game do
-
   use GenServer
-  alias IslandsEngine.{Game, Player,Coordinate,Island, IslandSet}
+  alias IslandsEngine.{Game, Player, Coordinate, Island, IslandSet}
   defstruct player1: :none, player2: :none
 
   # def handle_info(:first, state) do
@@ -18,20 +17,24 @@ defmodule IslandsEngine.Game do
     {:reply, :ok, state}
   end
 
-  def handle_call({:set_island_coordinates, player, island, coordinates}, _from, state)  do
-      state
-      |> Map.get(player)
-      |> Player.set_island_coordinates(island, coordinates)
-      {:reply, :ok, state}
+  def handle_call({:set_island_coordinates, player, island, coordinates}, _from, state) do
+    state
+    |> Map.get(player)
+    |> Player.set_island_coordinates(island, coordinates)
+
+    {:reply, :ok, state}
   end
 
   def handle_call({:guess, player, coordinate}, _from, state) do
     opponent = opponent(state, player)
     opponent_board = Player.get_board(opponent)
-    response = Player.guess_coordinate(opponent_board, coordinate)
+
+    response =
+      Player.guess_coordinate(opponent_board, coordinate)
       |> forest_check(opponent, coordinate)
       |> win_check(opponent, state)
   end
+
   def call_demo(game) do
     GenServer.call(game, :demo)
   end
@@ -48,30 +51,28 @@ defmodule IslandsEngine.Game do
   #   GenServer.cast(pid, :demo)
   # end
 
-
-  def start_link(name) when  is_binary(name) and byte_size(name) > 0 do
+  def start_link(name) when is_binary(name) and byte_size(name) > 0 do
     GenServer.start_link(__MODULE__, name, name: {:global, "game:#{name}"})
   end
 
   def init(name) do
     {:ok, player1} = Player.start_link(name)
     {:ok, player2} = Player.start_link()
-    {:ok, %Game{ player1: player1, player2: player2}}
+    {:ok, %Game{player1: player1, player2: player2}}
   end
-
 
   def add_player(pid, name) when name != nil do
     GenServer.call(pid, {:add_player, name})
   end
 
-  def set_island_coordinates(pid, player, island, coordinates) when is_atom player and is_atom island do
+  def set_island_coordinates(pid, player, island, coordinates)
+      when is_atom(player and is_atom(island)) do
     GenServer.call(pid, {:set_island_coordinates, player, island, coordinates})
   end
 
-
   def guess_coordinate(pid, player, coordinate)
-        when is_atom player and is_atom coordinate do
-          GenServer.call(pid, {:guess, player, coordinate})
+      when is_atom(player and is_atom(coordinate)) do
+    GenServer.call(pid, {:guess, player, coordinate})
   end
 
   defp opponent(state, :player) do
@@ -79,29 +80,31 @@ defmodule IslandsEngine.Game do
   end
 
   defp opponent(state, _player2) do
-      state.player1
+    state.player1
   end
+
   defp forest_check(:miss, _opponent, _coordinate) do
     {:miss, :none}
   end
 
   defp forest_check(:hit, opponent, coordinate) do
-      island_key = Player.forested_island(opponent, coordinate)
-      {:hit, island_key}
+    island_key = Player.forested_island(opponent, coordinate)
+    {:hit, island_key}
   end
 
   defp win_check({hit_or_miss, :none}, _opponent, state) do
     {:reply, {hit_or_miss, :none, :no_win}, state}
   end
+
   defp win_check({:hit, island_key}, opponent, state) do
     win_status =
       case Player.win?(opponent) do
-        true  -> :win
+        true -> :win
         false -> :no_win
       end
+
     {:reply, {:hit, island_key, win_status}, state}
   end
-
 
   def stop(pid) do
     GenServer.cast(pid, :stop)
